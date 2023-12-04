4498a656db15e21aaca5eafd5af42c76641b4737
a-t-0 <you@example.com>
2023-12-04 14:58:40 +0100

    Included assert_file_content_equal function.

--- src/helper_parsing.sh
+++ src/helper_parsing.sh
@@ -3,7 +3,25 @@
 assert_is_non_empty_string() {
   local string="$1"
   if [ "${string}" == "" ]; then
-    echo "Error, the incoming string was empty."
+    ERROR "Error, the incoming string was empty."
     exit 70
+  fi
+}
+
+# Function to assert file content
+assert_file_content_equal() {
+  local file_path="$1"
+  local expected_content="$2"
+
+  # Read the content of the file into a variable
+  local file_content
+  file_content=$(<"$file_path")
+
+  # Compare the content of the file with the expected content
+  if [ "$file_content" = "$expected_content" ]; then
+    echo "File content matches the expected content."
+  else
+    echo "File content does not match the expected content."
+    exit 1 # Or perform any other action as needed
   fi
 }
