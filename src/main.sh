2ffe953b58ec286bb138a9fd8ed51d544bc40e41
a-t-0 <you@example.com>
2023-12-05 21:33:29 +0100

    Renamed dependency functions.

--- src/main.sh
+++ src/main.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export bash_ssh_over_tor_is_loaded=true
+
 # Store arguments and then consume them to prevent the $@ argument from being
 # parsed in the wrong parser that is loaded through another main.sh file.
 CLI_ARGS_SSH_OVER_TOR=("$@")
